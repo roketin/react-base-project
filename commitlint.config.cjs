73ae3ff497bc0ee67cc9f2b061726a9538b9036e
Imam Stevanno <imam.stevanno@roketin.com>
2024-04-01 11:32:28 +0700

    feat: initial commit

---
+++ commitlint.config.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'chore',
+        'style',
+        'refactor',
+        'ci',
+        'test',
+        'revert',
+        'perf'
+      ]
+    ],
+    'type-case': [2, 'always', 'lowerCase'],
+    'type-empty': [2, 'never'],
+    'scope-case': [2, 'always', 'lowerCase']
+  }
+}
