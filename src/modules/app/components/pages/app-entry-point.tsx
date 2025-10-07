94bed5707997131262bcc74ddd4fa2de8169519a
Teguh Muhammad Zundi <teguh.muhammad@roketin.com>
2025-10-07 15:31:07 +0700

    feat(ddd):  entry point uses bootstrap loading to create consistency in the ux loading page

--- src/modules/app/components/pages/app-entry-point.tsx
+++ src/modules/app/components/pages/app-entry-point.tsx
@@ -1,7 +1,7 @@
 import { useNamedRoute } from '@/modules/app/hooks/use-named-route';
 import { useAuth } from '@/modules/auth/hooks/use-auth';
 import { useEffect } from 'react';
-import { RLoading } from '@/modules/app/components/base/r-loading';
+import { AppBootstrapLoading } from '@/modules/app/components/base/app-bootstrap-loading';
 
 const AppEntryPoint = () => {
   // Hooks
@@ -12,19 +12,13 @@ const AppEntryPoint = () => {
   useEffect(() => {
     const timeout = setTimeout(() => {
       navigate(isLoggedIn() ? 'DashboardIndex' : 'AuthLogin');
-    }, 300);
+    }, 200);
 
     return () => {
       clearTimeout(timeout);
     };
   }, [isLoggedIn, navigate]);
 
-  return (
-    <RLoading
-      label='App Skeleton'
-      className='h-full w-full'
-      labelClassName='text-lg font-semibold'
-    />
-  );
+  return <AppBootstrapLoading />;
 };
 export default AppEntryPoint;
