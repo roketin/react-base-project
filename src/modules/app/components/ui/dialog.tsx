286e0006468e6b31f6b7a1a6f4902cf0cb57553c
Teguh Muhammad Zundi <teguh.muhammad@roketin.com>
2025-12-05 08:29:28 +0700

    fix(ddd):  decrease and maintain z-index issue on dialog & alert-dialog

--- src/modules/app/components/ui/dialog.tsx
+++ src/modules/app/components/ui/dialog.tsx
@@ -28,7 +28,7 @@ const DialogOverlay = forwardRef<
       ref={ref}
       className={cn(
         { 'overflow-y-auto py-16 grid place-items-center': !fullscreen },
-        'fixed inset-0 z-9998 bg-black/40 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0',
+        'fixed inset-0 z-60 bg-black/40 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0',
         className,
       )}
       {...props}
